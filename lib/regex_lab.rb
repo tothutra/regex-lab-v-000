192e159baaf99e88fbb0ed7c8440ebc7ce26bfc2
Tra To <tothutra@gmail.com>
2018-08-10 04:36:06 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,7 +15,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  phone.match(/[1-9]\d{10}/) ? true : false
+  phone.match(/\d{10}/) ? true : false
 end
 
 
